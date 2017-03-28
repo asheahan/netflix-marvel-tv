72933e09546157e2c949281ab5e6d49d28da3f0b
Andrew Sheahan <asheahan@gmail.com>
2017-03-27 22:02:37 -0700

    Fix extract reviews to get ratings, date, and review body

--- scripts/extract_dd_reviews.py
+++ scripts/extract_dd_reviews.py
@@ -2,6 +2,12 @@
 import glob
 from bs4 import BeautifulSoup
 
+def get_ratings(row):
+    stars = float(len(row.select("span.glyphicon-star")))
+    if len(row.text.strip()) > 0:
+        stars += .5
+    return stars
+
 for season in range(1, 3):
     for file in glob.glob('data/daredevil/rottentomatoes/critic-reviews-season-' \
         + str(season) + '-*'):
@@ -22,8 +28,14 @@ for season in range(1, 3):
         with open(file, 'r') as handle:
             soup = BeautifulSoup(handle, "html.parser")
             for row in soup.select("div.review_table_row"):
-                review = {}
-                review['show'] = 'DareDevil'
-                review['season'] = str(season)
-                review['author'] = row.select("div.critic_name a")[0].text.strip()
-
+                if len(row.select("span.fl")) > 0:
+                    review = {}
+                    review['show'] = 'DareDevil'
+                    review['season'] = str(season)
+                    review['rating'] = get_ratings(row.select("span.fl")[0])
+                    review['author'] = row.select("div.critic_name a")[0].text.strip()
+                    review['date'] = row.select("span.fr")[0].text.strip() \
+                        if len(row.select("span.fr")) > 0 else ""
+                    review['body'] = row.select("div.user_review")[0].text.strip() \
+                        if len(row.select("div.user_review")) > 0 else ""
+                    print(review)
